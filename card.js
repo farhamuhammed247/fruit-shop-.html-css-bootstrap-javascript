4794af40782a4aeb5f9092db1a80dc57789d7726
Farha Sayed <78732985+farhamuhammed247@users.noreply.github.com>
2023-11-19 14:50:57 +0200

    Add files via upload

---
+++ card.js
@@ -0,0 +1,89 @@
+function deleteText() {
+  // Get the input element
+  var input = document.getElementById("text-to-delete");
+
+  // Check if the input element exists before deleting the text
+  if (input) {
+    // Delete the text by setting its value to an empty string
+    input.value = "";
+  }
+}
+function deleteText2() {
+    // Get the input element
+    var input = document.getElementById("text-to-delete2");
+  
+    // Check if the input element exists before deleting the text
+    if (input) {
+      // Delete the text by setting its value to an empty string
+      input.value = "";
+    }
+  }
+  function deleteText3() {
+    // Get the input element
+    var input = document.getElementById("text-to-delete3");
+  
+    // Check if the input element exists before deleting the text
+    if (input) {
+      // Delete the text by setting its value to an empty string
+      input.value = "";
+    }
+  }
+  function deleteText4() {
+    // Get the input element
+    var input = document.getElementById("text-to-delete4");
+  
+    // Check if the input element exists before deleting the text
+    if (input) {
+      // Delete the text by setting its value to an empty string
+      input.value = "";
+    }
+  }
+  function deleteText5() {
+    // Get the input element
+    var input = document.getElementById("text-to-delete5");
+  
+    // Check if the input element exists before deleting the text
+    if (input) {
+      // Delete the text by setting its value to an empty string
+      input.value = "";
+    }
+  }
+  function deleteText6() {
+    // Get the input element
+    var input = document.getElementById("text-to-delete6");
+  
+    // Check if the input element exists before deleting the text
+    if (input) {
+      // Delete the text by setting its value to an empty string
+      input.value = "";
+    }
+  }
+
+
+  function openModal() {
+    var modal = document.getElementById('cart');
+    var myModal = new bootstrap.Modal(modal);
+    myModal.show();
+  }
+
+ 
+
+  
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+  
